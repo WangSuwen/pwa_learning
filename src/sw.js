5d509f5f7f704f7fd44d83326150b4d6495b8bdb
wangxuquan <wangxuquan@meituan.com>
2018-09-14 15:25:54 +0800

    1

--- src/sw.js
+++ src/sw.js
@@ -2,7 +2,7 @@ importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.1.0/workbox
 var cacheStorageKey = 'minimal-pwa-1'
 var cacheList=[
   '/',
-  'index.html',
+  // 'index.html',
   'main.css',
   'daotong.jpeg'
 ]
